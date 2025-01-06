5e946b4e6c742fa16903751315487abe5119c719
Ruth Chiwendu Emmanuel <aruthu2003@gmail.com>
2025-01-05 22:07:09 -0500

    Day 1 task completed

---
+++ Day01.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+print("Day 1 - Write a simple Python script that prints Hello, World! and the results of basic arithmetic")
+print("- - -  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ")
+
+# A Python script that prints Hello, World! to the console
+print("Hello, World!")
+
+# Basic Arithmetic
+# Perform basic arithmetic if you've done this kind of thing before
+a = 5
+b = 3
+
+# Addition
+print(f"{a} + {b} = {a + b}")
+
+# Subtraction
+print(f"{a} - {b} = {a - b}")
+
+# Division
+print(f"{a} / {b} = {a / b}")
+
+# Multiplication
+print(f"{a} * {b} = {a * b}")
+
