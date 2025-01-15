a8d4f9f48e210ea9c18b0f26c61955a42555fac8
Ruth Chiwendu Emmanuel <aruthu2003@gmail.com>
2025-01-15 16:33:49 -0500

    Introduction to Libraries

---
+++ Day10.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+print ("Introductio to Libraries")
+print ("TASK: Write a Python script that calculates the square root of a number using the math library")
+
+import math
+
+# Take user input and change it to integer
+number = int(input("Enter a number: "))
+
+
+# Calculate the square root
+sqrt_result = math.sqrt(number)
+
+# Print the result
+print(f"The square root of {number} is {sqrt_result}")
+
+
