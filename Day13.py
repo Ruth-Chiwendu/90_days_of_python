e2c2d4f078a0f87aa45e55625960cac03b1b270f
Ruth Chiwendu Emmanuel <aruthu2003@gmail.com>
2025-01-19 20:08:29 -0500

    Using External Library:Request

---
+++ Day13.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+print ("Using External Libraries: Requests")
+print ("""Create a Python script that fetches data from a public API (e.g., OpenWeatherMap) and displays the 
+weather.""")
+
+import requests
+
+city_name = input ("Enter name of city: ")
+api_key ="56d92a1cbde2b81738c7fa01f9181cbc"
+url = url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric'
+
+response = requests.get(url)
+data = response.json()
+
+temperature = data["main"]["temp"]
+weather_description = data["weather"][0]["description"]
+
+print(f"Weather in {city_name}:")
+print(f"Temperature: {temperature}°C")
+print(f"Description: {weather_description}")
+
+
+
+
